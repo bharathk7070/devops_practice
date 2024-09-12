e37748b09739dc66a59f9a0f393da08f9af959e4
bharathk7070 <146103983+bharathk7070@users.noreply.github.com>
2024-09-12 20:21:49 +0530

    Create secret.js

---
+++ secret.js
@@ -0,0 +1,6 @@
+var config = {
+    bucket: "examplebucket",
+    awsKey: "AKIAIOSFODNN7EXAMPLE",
+    awsSecret: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+    server: "someserver.example.com"
+};
